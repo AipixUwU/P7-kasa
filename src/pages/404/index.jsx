375111f819944a8644f338cff0ab7f5df91cbf60
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-05-08 19:00:14 +0200

    Error 404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Error404() {
     return (
         <div className="error">
             <span className="error404">404</span>
             <span className="error-text">Oups! La page que vous demandez n'existe pas.</span>
+            <Link className="home-link" to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
 }
