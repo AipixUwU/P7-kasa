a6fadabba945821d494edab3e864d8e907914648
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-05-11 01:38:52 +0200

    Create AppRoutes file

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Housing from './pages/Housing/Housing';
+import About from './pages/About/About';
+import Error404 from './pages/404/404';
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/housing/:id" element={<Housing />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<Error404 />} />
+        </Routes>
+    )
+};
+
+export default AppRoutes;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/index.scss';
 import './styles/responsive.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Error404 from './pages/404/404';
-import Housing from './pages/Housing/Housing';
+import AppRoutes from './AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 	<React.StrictMode>
 		<Router>
 			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/housing/:id" element={<Housing />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/*" element={<Error404 />} />
-			</Routes>
+			<AppRoutes />
 			<Footer />
 		</Router>
-	</React.StrictMode>
-
+	</React.StrictMode>,
 );
