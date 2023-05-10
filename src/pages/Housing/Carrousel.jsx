5455ec1a3e7e7867b6e705cae98b485021c0f091
AipixUwU <113269618+AipixUwU@users.noreply.github.com>
2023-05-11 01:39:31 +0200

    Responsive

--- src/pages/Housing/Carrousel.jsx
+++ src/pages/Housing/Carrousel.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 function Carrousel({ images, alt }) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const lastImageIndex = images.length - 1;
 
     const goToNextImage = () => {
         setCurrentImageIndex((currentIndex) => (currentIndex === images.length - 1 ? 0 : currentIndex + 1));
@@ -19,6 +20,7 @@ function Carrousel({ images, alt }) {
             <div className="carrousel-controls">
                 <i className="fa-solid fa-chevron-left previousImage" onClick={goToPreviousImage}></i>
                 <i className="fa-solid fa-chevron-right nextImage" onClick={goToNextImage}></i>
+                <span className='image-counter'>{currentImageIndex +1}/{lastImageIndex +1}</span>
             </div>
         </div>
     )
